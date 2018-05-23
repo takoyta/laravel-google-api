789fcc2fb331fb2c8ee9ff2e6d496b35ac863482
Vladilen <blax56@gmail.com>
2018-05-23 17:40:24 +0300

    Added ContentID Provider

---
+++ src/Provider/Youtube/ContentID.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Websecret\LaravelGoogleApi\Provider\Youtube;
+
+use Websecret\LaravelGoogleApi\AbstractProvider;
+
+class ContentID extends AbstractProvider
+{
+
+    protected $url = 'https://content.googleapis.com/youtube/partner/v1';
+
+    /**
+     * @return array
+     */
+    public static function getAuthScopes()
+    {
+        return array_merge(parent::getAuthScopes(), [
+            'https://www.googleapis.com/auth/youtubepartner',
+        ]);
+    }
+
+    /**
+     * @param array $params
+     * @return mixed
+     */
+    public function getClaimSearchRequest($params = [])
+    {
+        return $this->getComputedRequest('GET', $this->buildQuery($this->url . '/claimSearch', $params));
+    }
+
+    /**
+     * @param mixed $id
+     * @param array $params
+     * @return mixed
+     */
+    public function getAssetRequest($id, $params = [])
+    {
+        return $this->getComputedRequest('GET', $this->buildQuery($this->url . '/assets/' . $id, $params));
+    }
+
+    /**
+     * @param array $params
+     * @return mixed
+     */
+    public function getAssetsListRequest($params = [])
+    {
+        return $this->getComputedRequest('GET', $this->buildQuery($this->url . '/assets', $params));
+    }
+
+    /**
+     * @param mixed $id
+     * @param array $params
+     * @return mixed
+     */
+    public function getContentOwnerRequest($id, $params = [])
+    {
+        return $this->getComputedRequest('GET', $this->buildQuery($this->url . '/contentOwners/' . $id, $params));
+    }
+}
